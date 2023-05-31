3157765a0dac8678119b45f6820d646a043913a1
Albert Albala <albert@mediatribe.net>
2023-05-31 13:46:36 -0400

    Please squash this commit in the pull request

---
+++ scripts/docker-host/switch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Switch code on a Docker host.
+#
+set -e
+
+echo "$ACQUIAAPP"
+echo "$ACQUIAENV"
+echo "$ACQUIATAG"
+echo "$ACLIKEY"
+echo "$ACLISECRET"
+echo "$DOCKERHOSTUSER"
+echo "$DOCKERHOST"
